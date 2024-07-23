c0e6f3c92b7130ffe7b105aab2dd9267c7c7b363
Tags01 <christiantag2012@gmail.com>
2024-07-22 23:29:29 -0400

    got preliminary unit test building; linker errors persist

--- pcl_filter_3d_utilities/tests/pcl_voxel_grid_filter/unit_tests.cpp
+++ pcl_filter_3d_utilities/tests/pcl_voxel_grid_filter/unit_tests.cpp
@@ -5,48 +5,54 @@
 #include <rclcpp/logger.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
 
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 #include "pcl_filter_3d_msgs/srv/pcl_voxel_grid_filter.hpp"
 
 #include "../ros2_service_test_fixture.hpp"
 
-using Fixture = ROS2ServiceTestFixture;
 using sensor_msgs::msg::PointCloud2;
 using pcl_filter_3d_msgs::srv::PCLVoxelGridFilter;
 
-static constexpr size_t TOPIC_COUNT = 30ZU;
+static constexpr size_t TOPIC_COUNT = 30U;
+using RequestArray = std::array<
+  PCLVoxelGridFilter::Request::SharedPtr, TOPIC_COUNT>;
+using ResponseArray = std::array<
+  PCLVoxelGridFilter::Response::SharedPtr, TOPIC_COUNT>;
 
 struct Fixture : public ROS2ServiceTestFixture {
   static std::string pointcloud_topic;
   static std::string service_topic;
 
   Fixture() {
-    pointcloud_topic = Fixture::get_node()->declare_parameter("camera_topic");
-    service_topic = Fixture::get_node()->declare_parameter("service_topic");
+    pointcloud_topic = Fixture::get_node()->declare_parameter<std::string>("camera_topic");
+    service_topic = Fixture::get_node()->declare_parameter<std::string>("service_topic");
   }
 };
 
+
 BOOST_GLOBAL_FIXTURE(Fixture);
+
+//BOOST_AUTO_TEST_SUITE(voxel_grid_filter_suite)
 
 BOOST_AUTO_TEST_CASE(test_output_nonzero) {
   auto messages = Fixture::get_cached_messages<PointCloud2, 30>(
     Fixture::pointcloud_topic);
 
-  std::array<PCLVoxelGridFilter::Request::SharedPtr, TOPIC_COUNT> requests;
-  std::array<PCLVoxelGridFilter::Response::SharedPtr, TOPIC_COUNT> resonses;
+  RequestArray requests;
+  ResponseArray responses;
 
-  size_t i;
-  for (size_t i = 0; i < ) {
-    request = std::make_shared<PCLVoxelGridFilter::Request>();
-    request->cloud_in = std::move(messages[i]);
-    i++;
+  for (size_t i = 0; i < messages.size(); i++) {
+    requests[i] = std::make_shared<PCLVoxelGridFilter::Request>();
+    requests[i]->cloud_in = std::move(messages[i]);
   }
 
-  Fixture::ros_service_transform(
+  Fixture::sync_send_requests_transform<PCLVoxelGridFilter>(
     Fixture::service_topic, requests.cbegin(),
-    requests.cend(), response.begin());
+    requests.cend(), responses.begin());
 
   for (const auto& response : responses) {
-    BOOST_ASSERT(messages.size() > 0);
+    BOOST_ASSERT(response->cloud_out.data.size() > 0);
   }
 }
+
+// BOOST_AUTO_TEST_SUITE_END()
--- pcl_filter_3d_utilities/tests/ros2_service_test_fixture.hpp
+++ pcl_filter_3d_utilities/tests/ros2_service_test_fixture.hpp
@@ -2,7 +2,7 @@
 #define SERVICE_TEST_FIXTURE_HEADER_ID_7632763864872641
 
 #include <rclcpp/wait_for_message.hpp>
-#include <rclcpp/lifecycle_node.hpp>
+#include <rclcpp/node.hpp>
 #include <rclcpp/client.hpp>
 #include <rclcpp/publisher.hpp>
 #include <rclcpp/subscription.hpp>
@@ -21,165 +21,160 @@
 #include <thread>
 #include <vector>
 #include <future>
-#include <mutex>
-#include <atomic>
-#include <condition_variable>
+#include <utility>
 
 #include <boost/test/included/unit_test.hpp>
-#include <boost/test/included/unit_test_framework.hpp>
 // need this because type information may be in an unreadable form otherwise
 #include <boost/core/demangle.hpp>
 
-template <typename MutexType = std::mutex>
-class FIFOMutex {
-  private:
-    MutexType mutex_;
-    std::atomic<std::optional<thread::id>> owner_;
-    std::queue<thread::id> thread_queue_;
-  public:
-    FIFOMutex(): mutex_(), owner_(std::nullopt), thread_queue_() {}
-    void lock() {
-      mutex_.lock();
-      thread_queue_.push_back(
-        std::this_thread::thread_id());
-    }
-
-    void unlock() {
-      if (thread_queue_.size() > 0) {
-        owner_.store(std::make_optional(thread_queue.front()));
-        thread_queue.pop_front();
-      }
-
-      mutex_.unlock();
-    }
-
-    bool try_lock() {
-      std::optional<std::thread_id> thread_id_;
-      thread_id_ = _atomic.get();
-      if (thread_id_ == std::this_thread::get_id())
-        return mutex_.try_lock();
-    }
-}
+static const std::unordered_map<rclcpp::FutureReturnCode, std::string> RCLCPP_FUTURE_STATUS_MAP {
+  {rclcpp::FutureReturnCode::SUCCESS, "SUCCESS"},
+  {rclcpp::FutureReturnCode::TIMEOUT, "TIMEOUT"},
+  {rclcpp::FutureReturnCode::INTERRUPTED, "INTERRUPTED"}};
 
 class ROS2ServiceTestFixture {
     private:
-      static rclcpp::NodeOptions node_options_ {};
-      static rclcpp::Node::SharedPtr node_ {nullptr};
-      static std::unordered_map<std::string, std::any> msgs_map_ {};
-      static std::thread node_thread_ {};
-      static std::mutex global_lock_ {};
+      static rclcpp::Executor::SharedPtr executor_;
+      static rclcpp::NodeOptions node_options_;
+      static rclcpp::CallbackGroup::SharedPtr callback_group_;
+      static rclcpp::Node::SharedPtr node_;
+      static std::unordered_map<std::string, std::any> msgs_map_;
     public:
-        ROS2ServiceTestFixture()
-        {
-            rclcpp::init(master_test_suite.argc, master_test_suite.argv);
-            node_options_.automatically_declare_parameters_from_overrides(true);
-            node_options_.allow_undeclared_parameters(true);
+      ROS2ServiceTestFixture()
+      {
+          const auto& master_test_suite = boost::unit_test::framework::master_test_suite();
+          rclcpp::init(master_test_suite.argc, master_test_suite.argv);
+          node_options_.automatically_declare_parameters_from_overrides(true);
+          node_options_.allow_undeclared_parameters(true);
 
-            node_ = std::make_shared<rclcpp::Node>("filter_node", "test", node_options_);
+          executor_ = std::make_shared<rclcpp::executors::MultiThreadedExecutor>();
 
-            const auto& master_test_suite = boost::unit_test::framework::master_test_suite();
-            //boost::unit_test::unit_test_log.set_format(node->get_logger());
+          node_ = std::make_shared<rclcpp::Node>("filter_node", "test", node_options_);
+          executor_->add_node(node_);
+          
+          callback_group_ = node_->create_callback_group(
+            rclcpp::CallbackGroupType::Reentrant);
 
-            //spin the node in the background while the tests run
-            node_thread_ = std::thread(std::bind(
-              run_node_main, master_test_suite.argc, master_test_suite.argv));
-        }
+          msgs_map_ = std::unordered_map<std::string, std::any>();
+          //boost::unit_test::unit_test_log.set_format(node->get_logger());
+      }
 
-        static void run_node_main(int argc, char ** argv) {
-          using std::chrono_literals::operator""ms;
-          while (rclcpp::ok()) {
-            global_lock_.lock();
-            rclcpp::spin_some(lock);
-            global_lock_.unlock();
-            std::this_thread::sleep_for(10ms);
-          };
-        }
+      virtual ~ROS2ServiceTestFixture() {
+        //boost::unit_test::unit_test_log::set_strean(std::cout);
 
-        virtual ~ROS2ServiceTestFixture() {
-          //boost::unit_test::unit_test_log::set_strean(std::cout);
-          std::lock_guard _guard(global_lock_);
-
-          rclcpp::shutdown(); // shutdown node
-          node_thread_.join(); // wait for thread to finish
-        }
+        rclcpp::shutdown(); // shutdown node
+      }
 
 
-        /**
-         *
-         * @decription:
-         *  This function supports recording a consistant fixed number of messages that
-         *  will remain the same for the input combination.
-         *
-         * The NUMBER size type if fixed since the number of messages retrieved each
-         * time should be fixed. If a variable number of messages were allowed and a
-         * first test requested 5 messages and another requested 10. The state of the message
-         * cache would change in a way that would introduce side-effects.
-         *
-         * RTTI is used to detect issues with types at runtime since no C++ facility exists
-         * to detect errors at compile time.
-         * boost::core::demangle is used to extract the type in a human readable format.
-         *
-         * @purpose
-         *  This function is used to minimize boilerplate in testing code
-         *
-         */
-        template<typename Message, size_t NUMBER>
-        static const std::array<Message, NUMBER>& get_cached_messages(
-          std::string topic,
-          std::chrono::seconds wait = std::chrono::seconds(-1)
-        ) {
-            using MsgArray = std::array<Message, NUMBER>;
+      /**
+       *
+       * @decription:
+       *  This function supports recording a consistant fixed number of messages that
+       *  will remain the same for the input combination.
+       *
+       * The NUMBER size type if fixed since the number of messages retrieved each
+       * time should be fixed. If a variable number of messages were allowed and a
+       * first test requested 5 messages and another requested 10. The state of the message
+       * cache would change in a way that would introduce side-effects.
+       *
+       * RTTI is used to detect issues with types at runtime since no C++ facility exists
+       * to detect errors at compile time.
+       * boost::core::demangle is used to extract the type in a human readable format.
+       *
+       * @purpose
+       *  This function is used to minimize boilerplate in testing code
+       *
+       */
+      template<typename Message, size_t NUMBER>
+      static const std::array<Message, NUMBER>& get_cached_messages(
+        std::string topic,
+        std::chrono::seconds wait = std::chrono::seconds(-1)
+      ) {
+          using MsgArray = std::array<Message, NUMBER>;
 
-            if (msgs_map_.count(topic) == 0) {
-                MsgArray msgs{};
+          if (msgs_map_.count(topic) == 0) {
+              MsgArray msgs;
 
-                for (auto& msg: msgs) {
-                  rclcpp::wait_for_message<Message>(msg, node_, topic, wait);
-                }
-                msgs_map_[topic] = std::make_any(std::move(msgs));
-                return msgs_map_[topic];
-            }
+              for (auto& msg: msgs) {
+                rclcpp::wait_for_message<Message>(msg, node_, topic, wait);
+              }
 
-            // allow case of bad cast to fallthrough
-            try {
-              return std::any_cast<MsgArray>(msgs_map_[topic]);
-            } catch (std::bad_any_cast& error) {
-              using std::string_literals::operator""s;
+              msgs_map_[topic] = std::any(std::move(msgs));
+              return std::any_cast<MsgArray&>(msgs_map_[topic]);
+          }
 
-              std::string incorrect_type = std::quoted(boost::core::demangle(typeid(MsgArray).name()));
-              std::string actual_type = std::quoted(boost::core::demangle(msgs_map_[topic].name()));
+          // allow case of bad cast to fallthrough
+          try {
+            return std::any_cast<MsgArray&>(msgs_map_[topic]);
+          } catch (std::bad_any_cast& error) {
+            using std::string_literals::operator""s;
 
-              BOOST_ERROR(node->get_logger_(), "the passed type "s + incorrect_type + " does not match " + actual_type)
-            }
-        }
+            std::string incorrect_type = boost::core::demangle(typeid(MsgArray).name());
+            std::string actual_type = boost::core::demangle(msgs_map_[topic].type().name());
+
+            BOOST_ERROR("the passed type "s + incorrect_type + " does not match " + actual_type + "\n" + error.what());
+            
+            throw std::logic_error("unreachable statement");
+          }
+      }
+
+    template<typename ServiceType>
+    static typename ServiceType::Response::SharedPtr sync_send_request(
+      std::string service_name,
+      typename ServiceType::Request::SharedPtr request,
+      std::chrono::seconds service_wait = std::chrono::seconds(-1),
+      std::chrono::seconds call_wait = std::chrono::seconds(-1))
+    {
+      typename rclcpp::Client<ServiceType>::SharedPtr client =
+        node_->create_client<ServiceType>(service_name);
+      client->wait_for_service(service_wait);
+      BOOST_ASSERT(client->service_is_ready());
+
+      std::shared_future future = client->async_send_request(request);
+      rclcpp::FutureReturnCode future_code =
+        executor_->spin_until_future_complete(future, call_wait);
+      BOOST_REQUIRE_EQUAL(RCLCPP_FUTURE_STATUS_MAP.at(future_code), "SUCCESS");
+      return future.get();
+    }
 
     template<typename ServiceType, typename InputIterator, typename OutputIterator>
-    static void ros_service_transform(
+    static OutputIterator sync_send_requests_transform(
         const std::string& service_name,
         InputIterator begin, InputIterator end,
         OutputIterator dest_start,
         std::chrono::seconds service_wait = std::chrono::seconds(-1),
         std::chrono::seconds call_wait = std::chrono::seconds(-1))
     {
-      rclcpp::Client::SharedPtr client = node_->create_client<ServiceType>(service_name);
-      //client->wait_for_service(service_wait);
+      typename rclcpp::Client<ServiceType>::SharedPtr
+        client = node_->create_client<ServiceType>(service_name);
+      client->wait_for_service(service_wait);
+      BOOST_ASSERT(client->service_is_ready());
 
-      std::vector<std::future<ResponseType>> awaitable_responses;
+      std::vector<typename rclcpp::Client<ServiceType>::FutureAndRequestId>
+        awaitable_responses;
 
       for (InputIterator request_iter = begin; request_iter != end; request_iter++) {
-        awaitable_responses.push_back(client->call_async(request_iter));
+        awaitable_responses.push_back(client->async_send_request(*request_iter));
       }
+
+      auto future_awaiter = [&call_wait](auto&& future) {
+          rclcpp::FutureReturnCode result = executor_->spin_until_future_complete(future, call_wait);
+          BOOST_REQUIRE_EQUAL(RCLCPP_FUTURE_STATUS_MAP.at(result), "SUCCESS");
+
+          return future.get();
+      };
 
       return std::transform(
         awaitable_responses.begin(), awaitable_responses.end(),
-        dest_start,
-        [&call_wait](auto& future) {
-            return future.wait_for(call_wait);
-        });
+        dest_start, future_awaiter);
     }
 
     static rclcpp::Node::SharedPtr get_node() {
       return node_;
+    }
+
+    static rclcpp::Executor::SharedPtr get_executor() {
+      return executor_;
     }
 };
 
