aac614fc4f4e1f37fe51374c1df3dd8933f54059
Tags01 <christiantag2012@gmail.com>
2024-08-21 17:33:10 -0400

    CFI & Thread Fail, Safe-stack & undefined & address & leak pass Got CFI safe stack working. New attribute to specify the linker as ld does not support LTO

--- pcl_utilities/build_target_option_flags.cmake
+++ pcl_utilities/build_target_option_flags.cmake
@@ -5,7 +5,6 @@ include_guard(GLOBAL)
 cmake_policy(SET CMP0069 NEW)
 include(CheckIPOSupported)
 
-
 #[[
 Clang Based Compiler and GNU:
   Syntax:
@@ -46,6 +45,18 @@ Omitted Flags:
   --fstack-protector-all = not designed for debugging, little gain from use
   glib assertion definitions = issues when linking with other executables with those flags off.
 ]]#
+
+function (btof_get_scoped_global OUTPUT POSTFIX)
+  if (DEFINED ${PROJECT_NAME}_${POSTFIX})
+    set(${OUTPUT} ${PROJECT_NAME}_${POSTFIX} PARENT_SCOPE)
+  elseif(DEFINED ENV{${PROJECT_NAME}_${POSTFIX}})
+    set(${OUTPUT} $ENV{${PROJECT_NAME}_${POSTFIX}} PARENT_SCOPE)
+  elseif(DEFINED DEFAULT_${POSTFIX})
+    set(${OUTPUT} ${DEFAULT_${POSTIX}} PARENT_SCOPE)
+  elseif(DEFINED ENV{DEFAULT_${POSTFIX}})
+    set(${OUTPUT} $ENV{DEFAULT_${POSTFIX}} PARENT_SCOPE)
+  endif()
+endfunction()
 
 set(BTOF_CLANGLIKE_MATCH_REGEX "(Clang|IntelLLVM|AppleClang|ARMClang|XLClang|IBMClang)")
 set(BTOF_GNULIKE_MATCH_REGEX "(GNU|Clang|IntelLLVM|AppleClang|ARMClang|XLClang|IBMClang)")
@@ -102,68 +113,96 @@ function(btof_default_gnulike_debug TARGET VISIBILITY LANG)
   endif()
 endfunction()
 
-function (btof_add_sanitizers TARGET VISIBILITY LANG)
-  # Would be good to support sanitizers per project level, ie. ${PROJECT_NAME}_SANITIZERS=...
+function(btof_gnulike_use_linker TARGET VISIBILITY LANG)
+  cmake_parse_arguments("ARGV" "" "LINKER;FAILURE" "" ${ARGN})
 
-  set (SANITIZERS)
-  if (NOT "${ARGN}" STREQUAL "")
-    list(APPEND SANITIZERS ${ARGN})
-  elseif(DEFINED ${PROJECT_NAME}_SANITIZERS)
-    list(APPEND SANITIZERS ${${PROJECT_NAME}_SANITIZERS})
-  elseif(DEFINED ENV{${PROJECT_NAME}_SANITIZERS})
-    list(APPEND SANITIZERS $ENV{${PROJECT_NAME}_SANITIZERS})
-  elseif(DEFINED SANITIZERS)
-    list(APPEND SANITIZERS ${SANITIZERS})
-  elseif(DEFINED ENV{SANITIZERS})
-    list(APPEND SANITIZERS ${SANITIZERS})
+  if (NOT DEFINED ARGV_LINKER)
+    btof_get_scoped_global(ARGV_LINKER "LINKER")
   endif()
 
-  # format the provided sanitizers so they can be passed to clang
-  string(REGEX REPLACE "[\n \t\r]" "" SANITIZERS "${SANITIZERS}")
-  string(REGEX REPLACE "[:; ,]+" "," SANITIZERS "${SANITIZERS}")
-  string(TOLOWER "${SANITIZERS}" SANITIZERS)
-  message(WARNING "SANITIZERS are ${SANITIZERS}")
+  if (NOT DEFINED ARGV_LINKER AND DEFINED ARGV_FAILURE)
+    set(${ARGV_FAILURE}
+      "Must be provided to use `-fuse-ld` on the target '${TARGET}'\n"
+      "Please provide an argument 'LINKER' or specify a environment/build "
+      "variable named ${PROJECT_NAME}_LINKER or GLOBAL_LINKER" PARENT_SCOPE)
+  endif()
 
-  if ("${SANITIZERS}" STREQUAL "")
+  if (DEFINED ARGV_LINKER AND CMAKE_${LANG}_COMPILER_ID MATCHES "${BTOF_GNULIKE_MATCH_REGEX}")
+    target_link_options(${TARGET} ${VISIBILITY} -fuse-ld=${ARGV_LINKER})
+    set_property(
+      TARGET ${TARGET} APPEND PROPERTY STATIC_LIBRARY_OPTIONS -fuse-ld=${LINKER})
+  endif()
+endfunction()
+
+function (btof_add_sanitizers TARGET VISIBILITY LANG)
+  cmake_parse_arguments("ARGV" "" "FAILURE" "SANITIZERS" ${ARGN})
+  if (NOT DEFINED ARGV_SANITIZERS)
+    btof_get_scoped_global(ARGV_SANITIZERS "SANITIZERS")
+  endif()
+
+  string(REGEX REPLACE "[,\t\n\r;:]+" "," SANITIZERS "${ARGV_SANITIZERS}")
+  string(TOLOWER "${SANITIZERS}" SANITIZERS)
+
+  if (NOT SANITIZERS OR SANITIZERS STREQUAL "")
+    if (DEFINED ARGV_FAILURE OR ON)
+      message(WARN #${ARGV_FAILURE}
+        "No Sanitizers are specified, please provide the 'SANITIZERS' named argument "
+        "or set the value as a build or environment variable")
+    endif()
     return()
   endif()
 
   if (NOT CMAKE_${LANG}_COMPILER_ID MATCHES "${BTOF_GNULIKE_MATCH_REGEX}")
-    message(WARNING
-      "Sanitizers could not be added to the target, '${TARGET}'"
-      "due to an unsupported compiler. Only GCC and CLANG-like compilers are supported"
-    )
+    if (DEFINED ARGV_FAILURE OR ON)
+      message(WARN #${ARGV_FAILURE}
+        "Sanitizers could not be added to the target, '${TARGET}'"
+        "due to an unsupported compiler. Only GCC and CLANG-like compilers are "
+        "supported" PARENT_SCOPE)
+    endif()
     return()
   endif()
 
-  # CFI sanitizer requires use with LTO
-  if (SANITIZERS STREQUAL "cfi")
+  set(SANITIZER_FLAGS)
+
+  # CFI sanitizer requires use with LTO and a proper linker
+  # This requires setting the visibility to hidden where possible and using a linker that is not lld
+  if (SANITIZERS MATCHES ".*cfi.*")
     check_ipo_supported(RESULT IS_IPO_SUPPORTED OUTPUT REASON LANGUAGES ${LANG})
+    btof_gnulike_use_linker(${TARGET} ${VISIBILITY} ${LANG} ${LINKER_PLUGIN} FAILURE LINK_FAIL_REASON)
+
     if (NOT IS_IPO_SUPPORTED)
-      message(WARNING "The CFI sanitizer cannot be used if lto is not enabled. LTO Could not be enabled for ${REASON}")
-      return()
+      message(WARNING ${ARGV_FAILURE}
+        "Sanitizers could not be added to the target '${target}' "
+        "Since LTO is not supported for the following reason: \n ${REASON}")
+      string(REGEX REPLACE "cfi[A-Za-z_\\-]*,?" "" SANITIZERS "${SANITIZERS}")
+    elseif (DEFINED LINK_FAIL_REASON)
+      message(WARNING
+        "A linker must be specified to use the 'cfi' sanitizer. "
+        "Skipping CFI SANITIZER: \n${REASON}")
+      string(REGEX REPLACE "cfi[A-Za-z_\\-]*,?" "" SANITIZERS "${SANITIZERS}")
+    else()
+      set_target_properties(${TARGET} PROPERTIES INTERPROCEDURAL_OPTIMIZATION ON)
+      list(APPEND SANITIZER_FLAGS -fvisibility=hidden -fsanitize-cfi-cross-dso)
     endif()
-
-    set_target_properties(${TARGET} PROPERTIES
-      INTERPROCEDURAL_OPTIMIZATION ON
-    )
-
-    set_property(
-      TARGET ${TARGET} APPEND PROPERTY STATIC_LIBRARY_OPTIONS
-      -flto-visibility=hidden)
   endif()
 
-  set(SANITIZER_FLAGS -fsanitize=${SANITIZERS} -fsanitize-recover)
+  message(WARNING "SANITIZERS: ${SANITIZERS}")
+
+  if (SANITIZERS STREQUAL "")
+    return()
+  endif()
+
+  list(APPEND SANITIZER_FLAGS -fsanitize=${SANITIZERS} -fsanitize-recover -fno-sanitize-trap)
 
   # fortify source cannot be higher than 0 when using sanitizers
   target_compile_options(${TARGET} ${VISIBILITY}
-    -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0 ${SANITIZER_FLAGS})
+    -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0 -fno-omit-frame-pointer
+    ${SANITIZER_FLAGS})
 
   target_link_options(${TARGET} ${VISIBILITY} ${SANITIZER_FLAGS})
   set_property(
     TARGET ${TARGET} APPEND PROPERTY STATIC_LIBRARY_OPTIONS
     ${SANITIZER_FLAGS})
-
 endfunction()
 
 # define which functions will be called in which order
@@ -172,21 +211,57 @@ set(BTOF_DEFAULT_COMPILE
   btof_default_gnulike_pedantic)
 set(BTOF_DEFAULT_DEBUG btof_default_debug_definitions
   btof_default_gnulike_debug)
+
 set(BTOF_DEFAULT_RELEASE
   btof_default_release_ipo
+  btof_gnulike_use_linker
   btof_default_gnulike_optimize)
 
 # Default options for building targets
-set(BTOF_DEFAULT ${BTOF_DEFAULT_COMPILE} ${BTOF_DEFAULT_DEBUG} ${BTOF_DEFAULT_RELEASE})
+set(BTOF_DEFAULT ${BTOF_DEFAULT_DEBUG} ${BTOF_DEFAULT_RELEASE})
 
-function(botf_target_configurations TARGET VISIBILITY LANG)
-  string(TOUPPER ${LANG} LANG)
+function(botf_target_configurations TARGET VISIBILITY)
+  cmake_parse_arguments("ARGV" "" "LANG" "FUNCTIONS;ARGS;FAILURE" ${ARGN})
 
-  if (NOT DEFINED CMAKE_${LANG}_COMPILER)
-    message(WARNING "A compiler unknown to CMake was specified, options include C, CXX, Fortran")
+  # Set the langauage of the executable if not CXX
+  if (NOT DEFINED ARGV_LANG)
+    set(ARGV_LANG CXX)
   endif()
 
-  foreach(CALLBACK ${ARGN})
-    cmake_language(CALL ${CALLBACK} ${TARGET} ${VISIBILITY} ${LANG})
+  get_property(LANGUAGES_STR GLOBAL PROPERTY ENABLED_LANGUAGES)
+  set(LANGUAGES ${LANGUAGES_STR})
+  if (NOT ARGV_LANG IN_LIST LANGUAGES)
+    message(SEND_ERROR
+      "The language '${ARGV_LANG}' on the target '${TARGET}' is not one "
+      "of the listed enabled languages: '${LANGUAGES}'"
+    )
+  endif()
+
+  if (NOT DEFINED ARGV_FUNCTIONS)
+    message(SEND_ERROR
+      "The FUNCTIONS argument must be provided")
+  endif()
+
+  if (NOT DEFINED ARGV_ARGS)
+    set(ARGV_ARGS)
+  endif()
+
+  set(FAILURE_REASONS)
+  foreach(CALLBACK ${ARGV_FUNCTIONS})
+    if (DEFINED)
+      unset(LOCAL_FAILURES)
+    endif()
+
+    if (COMMAND ${CALLBACK})
+      cmake_language(CALL ${CALLBACK} ${TARGET} ${VISIBILITY} ${ARGV_LANG} FAILURE LOCAL_FAILURES)
+    else()
+      message(SEND_ERROR "The FUNCTIONS argument '${CALLBACK}' is not a function or macro")
+    endif()
+
+    if (DEFINED LOCAL_FAILURES)
+      list(APPEND FAILURE_REASONS ${LOCAL_FAILURES})
+    endif()
   endforeach()
+
+  set(ARGV_FAILURE ${FAILURE_REASONS})
 endfunction()
